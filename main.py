11770b26f62666de15dede3f15393c493f22b713
gsingh52 <gds_52@yahoo.com>
2025-07-17 23:27:31 +0000

    version 1.0

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World version v:3.0'
+    return 'Welcome to Python Flask World version v:1.0'
 
 if __name__  == '__main__':
     app.run(host='0.0.0.0', port=8080)
